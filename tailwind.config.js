d1663b3d19a448d86f4ed398330e37e88f4ca44d
ChessMastermind <iegormaistriuk@gmail.com>
2026-01-24 17:31:42 +0200

    updated edexcel upto 2025 Oct session fixed issues with running locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./*.js",
+    "./*.html",
+    "./CIE/**/*.html",
+    "./Edexcel/**/*.html",
+    "./OCR/**/*.html",
+    "./OxfordAqa/**/*.html",
+    "./Privacy/**/*.html",
+    "./Disclaimer/**/*.html",
+    "./Contact/**/*.html"
+  ],
   theme: {
     extend: {},
   },
